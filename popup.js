0d492d89768f46773f8e5399f36eacdc02bdb682
Kia Farhang <kfarhang0@gmail.com>
2016-11-02 16:23:27 -0700

    Added functionality to put priority on other people's boards

--- popup.js
+++ popup.js
@@ -1,16 +1,29 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', populateUsers);
 document.getElementById('send').addEventListener('click', sendPriority);
 
 function sendPriority() {
 
     var title = document.getElementById('priority').value;
+
+    if (title == '') {
+        remindTitle();
+        return;
+    }
+
     var description = document.getElementById('description').value;
     var dueDate = document.getElementById('dueDate').value.toString();
+    var user_id;
 
-    var priority = 'title=' + title + '&description=' + description + '&due=' + dueDate;
+    if (document.getElementById('userSelect').value != 'Me') {
+        user_id = document.getElementById('userSelect').value;
+    }
 
-    chrome.storage.sync.get('key', function(data) { 
+
+    var priority = 'title=' + title + '&description=' + description + '&due=' + dueDate + '&user_id=' + user_id;
+
+    chrome.storage.sync.get('key', function(data) {
 
         var APIkey = data.key;
 
@@ -21,13 +34,10 @@ function sendPriority() {
 
 
         request.addEventListener('load', function() {
-           document.getElementById('priority').value = '';
-           document.getElementById('description').value = '';
-           document.getElementById('dueDate').value = '';
+            clearFields();
         });
 
         request.addEventListener('error', function() {
-            alert('nah');
             alert(request.responseText);
         });
 
@@ -36,4 +46,80 @@ function sendPriority() {
     });
 
 
+    function remindTitle() {
+        document.getElementById('priority').focus();
+        var statusArea = document.getElementById('titleCheck');
+        statusArea.innerText = 'Please enter a priority title';
+        setTimeout(function() {
+            statusArea.innerText = '';
+        }, 2000);
+        return;
+    }
+
 }
+
+
+function populateUsers() {
+
+    chrome.storage.sync.get('key', function(data) {
+
+        var APIkey = data.key;
+
+        var userRequest = new XMLHttpRequest();
+        userRequest.open('GET', 'https://www.yallahq.com/api/v1/user?');
+        userRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        userRequest.setRequestHeader('API_KEY', APIkey);
+
+        userRequest.addEventListener('load', function() {
+            var userObject = JSON.parse(userRequest.responseText);
+
+            // alert(userObject[0].name);
+            // alert(userObject[0].user_id);
+
+            for (var i = 0; i < userObject.length; i++) {
+                var userName = userObject[i].name;
+                var userID = userObject[i].user_id;
+
+                var option = document.createElement('option');
+                option.setAttribute('value', userID);
+                option.innerText = userName;
+
+                document.getElementById('userSelect').appendChild(option);
+            }
+        })
+
+        userRequest.addEventListener('error', function() {
+            alert(userRequest.responseText);
+        })
+
+        userRequest.send();
+
+    });
+
+
+
+    // var selectBox = document.getElementById('userSelect');
+
+    // for (var i = 0; i < 10; i++) {
+
+    //     var val = i * 5;
+
+    //     var option = document.createElement('option');
+    //     option.setAttribute('value', val);
+    //     option.innerText = val;
+    //     selectBox.appendChild(option);
+    // }
+
+}
+
+
+
+function clearFields() {
+    document.getElementById('priority').value = '';
+    document.getElementById('description').value = '';
+    document.getElementById('dueDate').value = '';
+    document.getElementById('userSelect').selectedIndex = 0;
+}
+
+
+// user_id : to send a priority to someone else
